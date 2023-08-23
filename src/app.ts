6e7d7625ebf9bb59ec4f6125084fa6a782503aea
im-allan <al.silvaquiroz@gmail.com>
2023-08-23 17:19:57 -0400

    feat: Access to views with handlebars was updated

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,35 @@
 const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
 const app = express()
-const port = 3000
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Middleware
 app.use(express.static('src/public'));
 
 app.get('/', function (req:any, res:any) {
-  res.send('index.html')
+  res.render('../src/views/home', {
+    name: 'Allan',
+    title: 'Curso de Node'
+  });
 })
 
-app.get('*', function (req:any, res:any) {
-  res.sendFile(__dirname +'/public/404.html');
+app.get('/generic', function (req:any, res:any) {
+  res.render('../src/views/generic', {
+    name: 'Allan',
+    title: 'Curso de Node'
+  });
+})
+
+app.get('/elements', function (req:any, res:any) {
+  res.render('../src/views/elements', {
+    name: 'Allan',
+    title: 'Curso de Node'
+  });
 })
 
 app.listen(port, () => {
